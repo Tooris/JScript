0bfe8ae3d8e4269c4d95a6556af993a8103eac4d
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 02:52:01 +0800

    Update UnlockVUEPro.js

--- SurgeJScript/UnlockVUEPro.js
+++ SurgeJScript/UnlockVUEPro.js
@@ -1,6 +1,6 @@
 let url = $request.url;
 let body = JSON.parse($response.body);
-let user = '/api/v1/users';
+let user = '/api/v1/users/self';
 if(url.index0f(user) != -1)
 {
 	body.data.isPremium = true;
