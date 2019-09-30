d1ac696010968f975c37ffe0fc0399faa13d225a
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 05:51:22 +0800

    Update UnlockVUEPro.js

--- Surge/UnlockVUEPro.js
+++ Surge/UnlockVUEPro.js
@@ -5,7 +5,7 @@ const pro = '/api/v1/users/';
 
 if(url.indexOf(pro) != -1){
   let obj = JSON.pares(body);
-  obj["pro"] = true;
+  obj["isPremium"] = true;
   body = JSON.stringify(obj);
 }
 $done({body});
