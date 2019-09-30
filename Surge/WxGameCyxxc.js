37cf263496627a81b54031968ec7e9b35fd82167
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 06:16:02 +0800

    Update WxGameCyxxc.js

--- Surge/WxGameCyxxc.js
+++ Surge/WxGameCyxxc.js
@@ -14,8 +14,8 @@ const getTip = '/game/player/reenter';
 if(url.indexOf(getTip) != -1)
 {
   let obj = JSON.parse(body);
-  obj.data.eConf.getTip = 999999999;
-  obj.data.eConf.shareTHigh = 9999;
+  obj.data.eConf.["getTip"] = 999999999;
+  obj.data.eConf.["shareTHigh"] = 9999;
   body = JSON.stringify(obj);
 }
 $done({body});
