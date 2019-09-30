621b0a297702bcef70eb2efe987c66ffd94a521b
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 05:16:18 +0800

    Update UnlockTickLitePro.js

--- Surge/UnlockTickLitePro.js
+++ Surge/UnlockTickLitePro.js
@@ -1,4 +1,17 @@
-let obj = JSON.parse($response.body);
-obj["pro"] = true;
-obj["proEndDate"] = "2099-12-30T00:00:00.000+0000";
+const v2 = '/api/v2/user/status';
+const v3 = '/api/v3/user/ranking';
+var url = $request.url;
+if(url.indexOf(v2) != -1){
+  let obj = JSON.parse($response.body);
+  obj["pro"] = true;
+  obj["proEndDate"] = "2099-12-30T00:00:00.000+0000";
+}
+
+if(url.indexOf(v3) != -1){
+ let obj = JSON.parse($response.body);
+  obj["level"] = 99;
+  obj["score"] = 50000;
+  
+}
+
 $done({body: JSON.stringify(obj)});
