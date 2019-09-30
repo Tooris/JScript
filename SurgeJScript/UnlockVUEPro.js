4993bdfd33f1f4206c162b984d2388daf3ff4ff3
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 03:53:23 +0800

    Update UnlockVUEPro.js

--- SurgeJScript/UnlockVUEPro.js
+++ SurgeJScript/UnlockVUEPro.js
@@ -1,13 +1,11 @@
 const path1 = '/api/v1/users/';
-
 var body = $response.body;
 var url = $request.url;
-if(url.index0f(path1) != -1)
-{
-	let obj = JSON.parse(body);
-	obj.data.isPremium = true;
-	obj.data.location = "在你妈肚子里";
+let obj = JSON.parse(body);
+if(url.index0f(path1) != -1){
+	obj["isPremium"] = true;
 	body = JSON.stringify(obj);
 }
 $done({body});
+
 
