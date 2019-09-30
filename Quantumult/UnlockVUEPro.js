9a5c1a59d521db67546b809e7d5a8cc0e0f18212
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 03:57:16 +0800

    Update UnlockVUEPro.js

--- Quantumult/UnlockVUEPro.js
+++ Quantumult/UnlockVUEPro.js
@@ -1,7 +1,7 @@
 let url = $request.url;
 let body = JSON.parse($response.body);
 let isPremium = '/api/v1/users/self';
-if(url.index0f(isPremium) != true)
+if(url.index0f(isPremium) != -1)
 {
 	body.data.isPremium = true;
 }
