b34e48289f8eaf41ca56d1f9510d6164e71cfbde
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 04:45:22 +0800

    Update UnlockTickListPro.js

--- Quantumult/UnlockTickListPro.js
+++ Quantumult/UnlockTickListPro.js
@@ -5,7 +5,7 @@ const pro = '/api/v2/user/status';
 
 if(url.indexOf(pro) != -1){
 	let obj = JSON.parse(body);
-	obj.pro = true;
+	obj["pro"] = true;
 		body = JSON.stringify(obj);
 }
 $done({body});
