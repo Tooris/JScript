3d712d5a53dcb2f028d52de6c80d140baa16cb50
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-02 02:15:23 +0800

    Create imgGif.js

---
+++ Surge/imgGif.js
@@ -0,0 +1,14 @@
+/*
+1.Modify Pro expression to false
+
+*/
+var body = $response.body;
+var url = $request.url;
+const stickers = '/api/stickers.json';
+if(url.indexOf(body))
+{
+	let obj = JSON.parse(body);
+	obj.data.stickers.pro = false;
+	body = JSON.stringify(obj);
+}
+$done({body});
